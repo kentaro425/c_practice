8a7df50e5d9a43299864e5aa88be5cb23378e3fc
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-18 18:59:59 +0900

    []13

--- main.c
+++ main.c
@@ -1,13 +1,7 @@
 #include <stdio.h>
-/*
-    演算子を用いた計算のプログラム
-*/
- 
+
 int main()
 {
-    // 各種演算
-    printf("%d + %d = %d\n",5,2,5+2);               /* 足し算 */
-    printf("%d - %d = %d\n",5,2,5-2);               /* 引き算 */
-    printf("%d * %d = %d\n",5,2,5*2);               /* 掛け算 */
-    printf("%d / %d = %d 余り %d \n",5,2,5/2, 5 % 2); /* 割り算 */  
+    printf("helloWorld\n");
+    printf("日本語でも大丈夫");
 }
