e7dbb9236bad34ec03fc7ced45a85252193bedee
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-19 23:39:20 +0900

    2

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 int main()
 {
-    printf("helloWorld\n");
-    printf("日本語でも大丈夫");
+    printf("こんにちは。私の名前は%sです。\n年齢は%dです。\n", "山田太郎", 20);
+    printf("イニシャルは、%cです。\n", 'Y');
+    printf("%f + %f = %f\n", 1.2, 2.7, 1.2 + 2.7);
 }
