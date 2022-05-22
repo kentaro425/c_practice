e10b88d3b7a3f71a1e65eabb241b7a2402c88feb
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-22 20:36:04 +0900

    5-5

--- sample2.c
+++ sample2.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-int main (void){
-  int value;
-  value = 10;
-  printf("%d", value);
+int main(void)
+{
+  // int value;
+  // value = 10;
+  // printf("%d", value);
+  int apple = 5;
+  int orange = 10;
+  printf("%d", apple + orange);
   return 0;
 }
