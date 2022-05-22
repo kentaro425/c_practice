1535383b0b30dbdca8ec1bd8a01a9b3901ec1b6e
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-22 20:55:29 +0900

    5-9

--- sample2.c
+++ sample2.c
@@ -16,6 +16,9 @@ int main(void)
   // apple --;
   // printf("%d\n", apple);
   // printf("%f", 3.14 * 3 * 3);
-  printf("%d", (int)(3.14 * 3 * 3));
+  // printf("%d", (int)(3.14 * 3 * 3));
+  int suuti;
+  scanf("%d", &suuti);
+  printf("入力された値は %d です", suuti);
   return 0;
 }
