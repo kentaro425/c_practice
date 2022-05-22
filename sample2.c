c105d6ce005283aef203a48c3db2d63298f9b772
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-22 20:40:54 +0900

    5-6

--- sample2.c
+++ sample2.c
@@ -4,8 +4,16 @@ int main(void)
   // int value;
   // value = 10;
   // printf("%d", value);
-  int apple = 5;
-  int orange = 10;
-  printf("%d", apple + orange);
+  // int apple = 5;
+  // int orange = 10;
+  // printf("%d", apple + orange);
+  int apple = 10;
+  printf("%d\n", apple);
+  apple ++;
+  printf("%d\n", apple);
+  apple ++;
+  printf("%d\n", apple);
+  apple --;
+  printf("%d\n", apple);
   return 0;
 }
