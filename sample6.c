462f7a7d5abc0f195b16c7d108677da1d7138821
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-23 12:38:03 +0900

    6-3

---
+++ sample6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+  // int apple = 10;
+  // if (apple - 10)
+  //   printf("リンゴの数は0個ではありません");
+  int apple = 8;
+  if (apple != 10)
+    printf("リンゴの数は10個ではありません");
+  return 0;
+}
