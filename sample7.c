350d0380f666b8eada294673891bd9532a86d839
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-23 13:21:56 +0900

    7-5

--- sample7.c
+++ sample7.c
@@ -2,11 +2,20 @@
 
 int main(void)
 {
-  int i;
-  for (i = 1; i < 10; i++)
+  // int i;
+  // for (i = 1; i < 10; i++)
+  // {
+  //   printf("%d\n", i);
+  // }
+
+  int year = 1;
+  double money = 1000000;
+  while (money < 100000000)
   {
-    printf("%d\n", i);
+    year++;
+    money = (money + 600000) * 1.04;
   }
+  printf("%d年目：%f円となり、資産が1億円を超えます", year, money);
 
   return 0;
 }
