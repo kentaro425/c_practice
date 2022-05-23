9e52dd0ac8d6837ee7c021bffbb17ed5f66bf643
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-23 12:46:51 +0900

    6-5

--- sample6.c
+++ sample6.c
@@ -7,13 +7,22 @@ int main(void)
   // int apple = 8;
   // if (apple != 10)
   //   printf("リンゴの数は10個ではありません");
-  int suuti;
-  scanf("%d", &suuti);
-  if (suuti < 10)
-    printf("10未満です");
-  if (suuti > 10)
-    printf("10より大きいです");
-  if (suuti == 10)
-    printf("10と同じです");
+
+  // int suuti;
+  // scanf("%d", &suuti);
+  // if (suuti < 10)
+  //   printf("10未満です");
+  // if (suuti > 10)
+  //   printf("10より大きいです");
+  // if (suuti == 10)
+  //   printf("10と同じです");
+
+  // int suuti;
+  // scanf("%d", &suuti);
+  // if (suuti >= 5 && suuti <= 10)
+  //   printf("5〜10の間です");
+  // if (suuti < 5 || suuti > 10)
+  //   printf("5〜10の間ではありません");
+
   return 0;
 }
