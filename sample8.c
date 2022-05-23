a2ae733b11e4b4975e7333f78276aeece281ecaa
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-23 17:35:10 +0900

    8-5

--- sample8.c
+++ sample8.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
-int hello(void);
+// int hello(void);
+// int main(void)
+// {
+//   hello();
+//   return 0;
+// }
+
+// int hello(void)
+// {
+//   printf("ありがとう");
+//   return 0;
+// }
+int keisan(int kingaku);
 int main(void)
 {
-  hello();
+  keisan(1000);
   return 0;
 }
 
-int hello(void)
+int keisan(int kingaku)
 {
-  printf("ありがとう");
+  printf("%f", kingaku * 1.1);
   return 0;
 }
