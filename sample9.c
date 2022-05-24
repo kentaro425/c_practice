fba5d2d39cd9825a38116f135419fa35883c378a
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-24 09:01:13 +0900

    9-4

--- sample9.c
+++ sample9.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 
+// int main(void)
+// {
+
+//   int apple = 30;
+
+//   printf("%d", apple);
+//   return 0;
+// }
+
+// int fruits
+// {
+// }
+
+int apple = 10;
 int main(void)
 {
-
-  int apple = 30;
-
+  int apple = 5;
   printf("%d", apple);
   return 0;
-}
-
-int fruits{
-  
 }
