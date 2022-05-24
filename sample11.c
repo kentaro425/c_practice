f57bdfa96dc34179fe0e6a92a547f2603ae62f45
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-24 09:42:43 +0900

    11-4

--- sample11.c
+++ sample11.c
@@ -6,9 +6,16 @@
 //   return 0;
 // }
 
+// int main(void)
+// {
+//   char str[5] = {'J', 'O', 'H', 'N', '\0'};
+//   printf("%s", str);
+//   return 0;
+// }
+
 int main(void)
 {
-  char str[5] = {'J', 'O', 'H', 'N', '\0'};
-  printf("%s", str);
+  char str2[] = "JOHN";
+  printf("%s", str2);
   return 0;
 }
