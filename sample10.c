a2fa96bf234e959b69b20e1e00790ac957608ec0
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-24 09:19:04 +0900

    10-6

--- sample10.c
+++ sample10.c
@@ -20,11 +20,22 @@
 //   return 0;
 // }
 
+// int main(void)
+// {
+//   int array[] = {12, 13, 14};
+//   printf("%d\n", array[0]);
+//   printf("%d\n", array[1]);
+//   printf("%d\n", array[2]);
+//   return 0;
+// }
+
 int main(void)
 {
-  int array[] = {12, 13, 14};
-  printf("%d\n", array[0]);
-  printf("%d\n", array[1]);
-  printf("%d\n", array[2]);
+  int array[5] = {12, 13, 14};
+  int i;
+  for (i = 0; i < 5; i++)
+  {
+    printf("%d番目は%dです\n", i, array[i]);
+  }
   return 0;
 }
