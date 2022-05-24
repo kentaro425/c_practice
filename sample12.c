095b6bdf14ec7c73ab8a1b6d55899830831dec2d
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-24 16:48:28 +0900

    12-6

--- sample12.c
+++ sample12.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
+
+// int main(void)
+// {
+//   int apple = 10;
+//   int *hoge;
+//   hoge = &apple;
+//   printf("%d\n", apple);
+//   printf("%d\n", *hoge);
+//   return 0;
+// }
+
 int main(void)
 {
-  int apple = 10;
-  int *hoge;
+  int apple, *hoge;
   hoge = &apple;
-  printf("%d\n", apple);
-  printf("%d\n", *hoge);
+  printf("%p\n", &apple);
+  printf("%p\n", hoge);
   return 0;
 }
