0d919537d670b25a7cac1b166a7c33740fed24eb
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-05-24 09:12:41 +0900

    10-4

--- sample10.c
+++ sample10.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+// int main(void)
+// {
+//   int array[10];
+//   array[5] = 15;
+//   printf("%d\n", array[5]);
+//   printf("%d\n", array[4]);
+//   // printf("%d\n", array[20]);
+//   return 0;
+// }
+
 int main(void)
 {
-  int array[10];
-  array[5] = 15;
-  printf("%d\n", array[5]);
+  int array[5] = {12, 13, 14};
+  printf("%d\n", array[0]);
+  printf("%d\n", array[1]);
+  printf("%d\n", array[2]);
+  printf("%d\n", array[3]);
   printf("%d\n", array[4]);
-  // printf("%d\n", array[20]);
   return 0;
 }
